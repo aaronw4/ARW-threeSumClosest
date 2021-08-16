95518b8981d0bb43de0a7a119226ed342715619b
Aaron Williams <aaronrw4@yahoo.com>
2021-08-15 18:46:28 -0700

    Simple but slow solution

---
+++ threeSum2.js
@@ -0,0 +1,24 @@
+var threeSumClosest = function(nums, target) {
+    let condition = false
+    let difference = 0
+
+    while (condition !== true) {
+        for (i = 0; i < nums.length  - 2; i++) {
+            for (j = 1; j < nums.length - 1; j++) {
+                for (k = 2; k < nums.length; k++) {
+                    let sum = nums[i] + nums[j] + nums[k]
+                    if (sum - difference === target || sum + difference === target) {
+                        if (i !== j && i !== k && j !== k) {
+                            return (sum)
+                        } else {
+                            continue
+                        }                        
+                    }
+                }
+            }
+        }
+        difference++
+    }
+}
+
+console.log(threeSumClosest([-1,2,1,-4], 1))
